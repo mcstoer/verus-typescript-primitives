a4b96c048302492150237f595da699348a33d68e
michaeltout <toutonghi.michael@gmail.com>
2025-11-21 14:00:30 +0100

    Rename DataResponse, push build files

--- dist/vdxf/classes/ordinals/AppEncryptionRequestOrdinalVdxfObject.d.ts
+++ dist/vdxf/classes/ordinals/AppEncryptionRequestOrdinalVdxfObject.d.ts
@@ -1,9 +1,9 @@
 import { SerializableDataEntity } from "../../../utils/types/SerializableEntity";
 import { OrdinalVdxfObjectInterfaceTemplate, OrdinalVdxfObjectJsonTemplate } from "./OrdinalVdxfObject";
 import { SerializableEntityOrdinalVdxfObject } from "./SerializableEntityOrdinalVdxfObject";
-import { AppEncryptionRequestDetails, AppEncryptionRequestDetailsJson } from "../requestobjects/AppEncryptionRequestDetails";
+import { AppEncryptionRequestDetails, AppEncryptionRequestJson } from "../requestobjects/AppEncryptionRequestDetails";
 export declare class AppEncryptionRequestOrdinalVdxfObject extends SerializableEntityOrdinalVdxfObject implements SerializableDataEntity {
     data: AppEncryptionRequestDetails;
     constructor(request?: OrdinalVdxfObjectInterfaceTemplate<AppEncryptionRequestDetails>);
-    static fromJson(details: OrdinalVdxfObjectJsonTemplate<AppEncryptionRequestDetailsJson>): AppEncryptionRequestOrdinalVdxfObject;
+    static fromJson(details: OrdinalVdxfObjectJsonTemplate<AppEncryptionRequestJson>): AppEncryptionRequestOrdinalVdxfObject;
 }
